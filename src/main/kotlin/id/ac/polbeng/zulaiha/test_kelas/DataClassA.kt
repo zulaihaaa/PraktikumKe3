3f2a62fcb25b6a64f5643d8ccafac129dacdabb8
Siti Zulaiha <rupatsiti12@gmail.com>
2022-04-23 10:27:04 +0700

    Contoh Kode Program  Data Class

---
+++ src/main/kotlin/id/ac/polbeng/zulaiha/test_kelas/DataClassA.kt
@@ -0,0 +1,11 @@
+package id.ac.polbeng.zulaiha.test_kelas
+
+data class Student(val name: String, val age: Int)
+fun main() {
+    val boni = Student("Boni", 21)
+    val meri = Student("Meri", 20)
+    println("Student Name is: ${boni.name}")
+    println("Student Age is: ${boni.age}")
+    println("Student Name is: ${meri.name}")
+    println("Student Age is: ${meri.age}")
+}
