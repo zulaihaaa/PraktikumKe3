7eb7c4d661b6233e15b80ed2cdb7e251b8cdb981
Siti Zulaiha <rupatsiti12@gmail.com>
2022-04-23 10:30:33 +0700

    Contoh Kode Program  Data Class C

---
+++ src/main/kotlin/id/ac/polbeng/zulaiha/test_kelas/DataClassC_31.kt
@@ -0,0 +1,12 @@
+package id.ac.polbeng.zulaiha.test_kelas
+
+fun main(){
+    val meri = Student("Meri", 20)
+    val badu = meri.copy("Badu")
+    if(meri.equals(badu))
+        println("meri is equal to badu.")
+    else
+        println("meri is not equal to badu.")
+    println("Hashcode of meri: ${meri.hashCode()}")
+    println("Hashcode of badu: ${badu.hashCode()}")
+}
