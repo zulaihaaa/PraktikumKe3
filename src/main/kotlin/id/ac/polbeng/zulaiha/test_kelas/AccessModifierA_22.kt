1e4bffe31c3a01cf41deecedd3c329f0f547f557
Siti Zulaiha <rupatsiti12@gmail.com>
2022-04-23 10:07:15 +0700

    Contoh Kode Program AccessModifier A

---
+++ src/main/kotlin/id/ac/polbeng/zulaiha/test_kelas/AccessModifierA_22.kt
@@ -0,0 +1,32 @@
+package id.ac.polbeng.zulaiha.test_kelas
+
+open class Mahasiswa(var nama: String, var nrp: Int){
+    open fun info() {
+        println(nama +'\n'+ nrp);
+    }
+    override fun toString(): String {
+        return "Mahasiswa{nama= $nama, nrp= $nrp}"
+    }
+}
+class KetuaHima(nama: String, nrp: Int, val jurusan: String) : Mahasiswa(nama, nrp) {
+    @Override
+    override fun info(){
+        println(nama +'\n'+ nrp +'\n'+ jurusan);
+    }
+}
+fun main(){
+    val budi = Mahasiswa("Budi Gunawan", 1106123)
+    val anton = KetuaHima("Anton", 1106789, "Teknik Informatika")
+    println(anton.toString());
+    println()
+    anton.info()
+    println("Jenis Kelas = " + anton.javaClass.simpleName)
+    println()
+    budi.info()
+    println("Jenis Kelas = " + budi.javaClass.simpleName)
+    val ucok = KetuaHima("Ucok", 1105239, "Teknik Elektro")
+    println()
+    ucok.info()
+    println("Jenis Kelas = " + ucok.javaClass.simpleName)
+}
+
