1ab3545452e2366e510d4ef941e3795ef2d6cce6
Siti Zulaiha <rupatsiti12@gmail.com>
2022-04-23 10:34:16 +0700

    Contoh Kode Program  Data Class E

---
+++ src/main/kotlin/id/ac/polbeng/zulaiha/test_kelas/DataClassE_34.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.zulaiha.test_kelas
+
+fun main() {
+    val meri = Student("Meri", 20)
+    // Destructuring Declaration
+    val(name, age) = meri
+    println("Name = $name")
+    println("Age = $age")
+}
